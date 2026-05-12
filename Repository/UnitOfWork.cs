90f87bb8e75919d7ffdf58ccea6de8205301ea2c
JeHan <jajehan19@gmail.com>
2026-05-12 21:54:00 +0600

    Add files via upload

---
+++ Repository/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using LostAndFoundSystem.Data;
+using LostAndFoundSystem.Interfaces;
+using LostAndFoundSystem.Models;
+
+namespace LostAndFoundSystem.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        public IRepository<Role> Roles { get; private set; }
+        public IRepository<User> Users { get; private set; }
+        public IRepository<Item> Items { get; private set; }
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+            Roles = new Repository<Role>(context);
+            Users = new Repository<User>(context);
+            Items = new Repository<Item>(context);
+        }
+
+        public async Task<int> SaveAsync() =>
+            await _context.SaveChangesAsync();
+
+        public void Dispose() =>
+            _context.Dispose();
+    }
+}
