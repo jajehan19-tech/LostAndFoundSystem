3a5b167279336ea1493d4263d039b6b6ff656878
JeHan <jajehan19@gmail.com>
2026-05-12 21:29:19 +0600

    Add files via upload

---
+++ Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using LostAndFoundSystem.Models;
+
+namespace LostAndFoundSystem.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<Role> Roles { get; }
+        IRepository<User> Users { get; }
+        IRepository<Item> Items { get; }
+        Task<int> SaveAsync();
+    }
+}
